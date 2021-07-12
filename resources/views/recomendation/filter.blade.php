7b536a8f8b2f245c59a69c07c2cf781666655a9e
dodinovembri <dodinovembri32@gmail.com>
2021-07-12 19:32:00 +0700

    update

--- resources/views/recomendation/filter.blade.php
+++ resources/views/recomendation/filter.blade.php
@@ -86,7 +86,7 @@
                                                             </select>
                                                         <?php } ?>
                                                         <div class="subscribe-btn d-flex align-items-center">
-                                                            <button type="button" id="exampleModal" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary">
+                                                            <button type="button" id="myBtn" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary">
                                                                 <span class="button-text">Keterangan Kriteria</span>
                                                                 <span class="button-loader"><i class="fas fa-sync-alt"></i></span>
                                                             </button>
@@ -106,6 +106,106 @@
                                         </div>
                                     </div>
                                 </article>
+                                <style>
+                                    /* The Modal (background) */
+                                    .modal {
+                                        display: none;
+                                        /* Hidden by default */
+                                        position: fixed;
+                                        /* Stay in place */
+                                        z-index: 1;
+                                        /* Sit on top */
+                                        padding-top: 100px;
+                                        /* Location of the box */
+                                        left: 0;
+                                        top: 0;
+                                        width: 100%;
+                                        /* Full width */
+                                        height: 100%;
+                                        /* Full height */
+                                        overflow: auto;
+                                        /* Enable scroll if needed */
+                                        background-color: rgb(0, 0, 0);
+                                        /* Fallback color */
+                                        background-color: rgba(0, 0, 0, 0.4);
+                                        /* Black w/ opacity */
+                                    }
+
+                                    /* Modal Content */
+                                    .modal-content {
+                                        background-color: #fefefe;
+                                        margin: auto;
+                                        padding: 20px;
+                                        border: 1px solid #888;
+                                        width: 50%;
+                                    }
+
+                                    /* The Close Button */
+                                    .close {
+                                        color: #aaaaaa;
+                                        float: right;
+                                        font-size: 28px;
+                                        font-weight: bold;
+                                    }
+
+                                    .close:hover,
+                                    .close:focus {
+                                        color: #000;
+                                        text-decoration: none;
+                                        cursor: pointer;
+                                    }
+                                </style>
+
+
+                                <h2>Modal Example</h2>
+
+                                <!-- Trigger/Open The Modal -->
+                                <button id="myBtn">Open Modal</button>
+
+                                <!-- The Modal -->
+                                <div id="myModal" class="modal">
+
+                                    <!-- Modal content -->
+                                    <div class="modal-content">
+                                        <center><span class="close">&times;</span></center>
+                                        <?php foreach ($criterias as $key => $value) { ?>
+                                            <ul><b>{{ $value->nama_kriteria }}</b> ({{ $value->keterangan }})
+                                                <?php foreach ($value->criterion_value as $key2 => $value2) { ?>
+                                                    <li style="margin-left: 20px;">{{ $value2->keterangan }}</li>
+                                                <?php } ?>
+                                            </ul><br>
+                                        <?php } ?>
+                                    </div>
+
+                                </div>
+
+                                <script>
+                                    // Get the modal
+                                    var modal = document.getElementById("myModal");
+
+                                    // Get the button that opens the modal
+                                    var btn = document.getElementById("myBtn");
+
+                                    // Get the <span> element that closes the modal
+                                    var span = document.getElementsByClassName("close")[0];
+
+                                    // When the user clicks the button, open the modal 
+                                    btn.onclick = function() {
+                                        modal.style.display = "block";
+                                    }
+
+                                    // When the user clicks on <span> (x), close the modal
+                                    span.onclick = function() {
+                                        modal.style.display = "none";
+                                    }
+
+                                    // When the user clicks anywhere outside of the modal, close it
+                                    window.onclick = function(event) {
+                                        if (event.target == modal) {
+                                            modal.style.display = "none";
+                                        }
+                                    }
+                                </script>
 
                             </div>
 
